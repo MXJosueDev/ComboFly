1b3174d9042c3cd943dc59c2d1556725f4318355
MXJosue <69768362+MXJosueDev@users.noreply.github.com>
2021-11-07 20:08:48 -0600

    First commit of the code

---
+++ src/combofly/EventListener.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+/*
+ *   _____                _           ______ _       
+ *  / ____|              | |         |  ____| |      
+ * | |     ___  _ __ ___ | |__   ___ | |__  | |_   _ 
+ * | |    / _ \| '_ ` _ \| '_ \ / _ \|  __| | | | | |
+ * | |___| (_) | | | | | | |_) | (_) | |    | | |_| |
+ *  \_____\___/|_| |_| |_|_.__/ \___/|_|    |_|\__, |
+ *                                             __/ |
+ *                                            |___/ 
+ */
+
+namespace combofly;
+
+use pocketmine\event\Listener;
+
+class EventListener implements Listener {
+}
--- src/combofly/Loader.php
+++ src/combofly/Loader.php
@@ -1,1 +1,50 @@
 <?php
+declare(strict_types=1);
+
+/*
+ *   _____                _           ______ _       
+ *  / ____|              | |         |  ____| |      
+ * | |     ___  _ __ ___ | |__   ___ | |__  | |_   _ 
+ * | |    / _ \| '_ ` _ \| '_ \ / _ \|  __| | | | | |
+ * | |___| (_) | | | | | | |_) | (_) | |    | | |_| |
+ *  \_____\___/|_| |_| |_|_.__/ \___/|_|    |_|\__, |
+ *                                             __/ |
+ *                                            |___/ 
+ */
+
+namespace combofly;
+
+use combofly\commands\CommandManager;
+use combofly\utils\ConfigManager;
+use onebone\economyapi\EconomyAPI;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\utils\SingletonTrait;
+use pocketmine\utils\TextFormat as TF;
+
+class Loader extends PluginBase {
+    use SingletonTrait;
+
+    private $economy = null;
+    
+    public function onLoad() {
+        self::setInstance($this);        
+    }
+
+    public function onEnable() {
+        ConfigManager::saveAll();
+        CommandManager::registerAll();
+        
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+
+        if(!is_null($this->getServer()->getPluginManager()->getPlugin("EconomyAPI"))) {
+			self::$economy = EconomyAPI::getInstance();
+		} else {
+			self::$economy = null;
+		}
+    }
+
+    public function giveKill(Player $player): void {
+        // TODO
+    }
+}
